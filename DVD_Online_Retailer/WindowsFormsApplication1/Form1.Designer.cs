935222fb1474e6221f10f55718c2e915ca95fcba
ryan marsh <ryan marsh>
2017-10-25 12:21:21 -0400

    order entry form

--- DVD_Online_Retailer/WindowsFormsApplication1/Form1.Designer.cs
+++ DVD_Online_Retailer/WindowsFormsApplication1/Form1.Designer.cs
@@ -283,7 +283,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Aqua;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(792, 508);
             this.Controls.Add(this.AddDVDButton);
             this.Controls.Add(this.QuantityLabel);
