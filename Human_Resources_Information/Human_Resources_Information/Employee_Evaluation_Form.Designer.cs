c964f44869b3434dabf2a8872d8943d3547d6c4b
ryan marsh <ryan marsh>
2017-11-13 21:20:07 -0500

    created employee evaluation form

---
+++ Human_Resources_Information/Human_Resources_Information/Employee_Evaluation_Form.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Human_Resources_Information
+{
+    partial class Employee_Evaluation_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeeIDLabel = new System.Windows.Forms.Label();
+            this.EmployeeIDTextBox = new System.Windows.Forms.RichTextBox();
+            this.EvaluationDateLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ReviewerNameLabel = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.ReviewerTitleLabel = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.AttendanceLabel = new System.Windows.Forms.Label();
+            this.AttendanceComboBox = new System.Windows.Forms.ComboBox();
+            this.JobKnowldegeLabel = new System.Windows.Forms.Label();
+            this.JobKnowledgeComboBox = new System.Windows.Forms.ComboBox();
+            this.ProductivityLabel = new System.Windows.Forms.Label();
+            this.ProductividyComboBox = new System.Windows.Forms.ComboBox();
+            this.AttitudeLabel = new System.Windows.Forms.Label();
+            this.AttitudeComboBox = new System.Windows.Forms.ComboBox();
+            this.CommunicationSkillsLabel = new System.Windows.Forms.Label();
+            this.CommunicationSkillsComboBox = new System.Windows.Forms.ComboBox();
+            this.SumbitEvaluationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EmployeeIDLabel
+            // 
+            this.EmployeeIDLabel.AutoSize = true;
+            this.EmployeeIDLabel.Location = new System.Drawing.Point(24, 27);
+            this.EmployeeIDLabel.Name = "EmployeeIDLabel";
+            this.EmployeeIDLabel.Size = new System.Drawing.Size(67, 13);
+            this.EmployeeIDLabel.TabIndex = 0;
+            this.EmployeeIDLabel.Text = "Employee ID";
+            // 
+            // EmployeeIDTextBox
+            // 
+            this.EmployeeIDTextBox.Location = new System.Drawing.Point(27, 43);
+            this.EmployeeIDTextBox.Name = "EmployeeIDTextBox";
+            this.EmployeeIDTextBox.Size = new System.Drawing.Size(317, 28);
+            this.EmployeeIDTextBox.TabIndex = 1;
+            this.EmployeeIDTextBox.Text = "";
+            // 
+            // EvaluationDateLabel
+            // 
+            this.EvaluationDateLabel.AutoSize = true;
+            this.EvaluationDateLabel.Location = new System.Drawing.Point(24, 95);
+            this.EvaluationDateLabel.Name = "EvaluationDateLabel";
+            this.EvaluationDateLabel.Size = new System.Drawing.Size(97, 13);
+            this.EvaluationDateLabel.TabIndex = 2;
+            this.EvaluationDateLabel.Text = "Date Of Evaluation";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 111);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(317, 27);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // ReviewerNameLabel
+            // 
+            this.ReviewerNameLabel.AutoSize = true;
+            this.ReviewerNameLabel.Location = new System.Drawing.Point(27, 160);
+            this.ReviewerNameLabel.Name = "ReviewerNameLabel";
+            this.ReviewerNameLabel.Size = new System.Drawing.Size(95, 13);
+            this.ReviewerNameLabel.TabIndex = 4;
+            this.ReviewerNameLabel.Text = "Name of Reviewer";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(27, 177);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(317, 28);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // ReviewerTitleLabel
+            // 
+            this.ReviewerTitleLabel.AutoSize = true;
+            this.ReviewerTitleLabel.Location = new System.Drawing.Point(30, 226);
+            this.ReviewerTitleLabel.Name = "ReviewerTitleLabel";
+            this.ReviewerTitleLabel.Size = new System.Drawing.Size(82, 13);
+            this.ReviewerTitleLabel.TabIndex = 6;
+            this.ReviewerTitleLabel.Text = "Reviewer\'s Title";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(27, 243);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(317, 34);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // AttendanceLabel
+            // 
+            this.AttendanceLabel.AutoSize = true;
+            this.AttendanceLabel.Location = new System.Drawing.Point(27, 306);
+            this.AttendanceLabel.Name = "AttendanceLabel";
+            this.AttendanceLabel.Size = new System.Drawing.Size(62, 13);
+            this.AttendanceLabel.TabIndex = 8;
+            this.AttendanceLabel.Text = "Attendance";
+            // 
+            // AttendanceComboBox
+            // 
+            this.AttendanceComboBox.FormattingEnabled = true;
+            this.AttendanceComboBox.Location = new System.Drawing.Point(27, 323);
+            this.AttendanceComboBox.Name = "AttendanceComboBox";
+            this.AttendanceComboBox.Size = new System.Drawing.Size(317, 21);
+            this.AttendanceComboBox.TabIndex = 9;
+            // 
+            // JobKnowldegeLabel
+            // 
+            this.JobKnowldegeLabel.AutoSize = true;
+            this.JobKnowldegeLabel.Location = new System.Drawing.Point(446, 27);
+            this.JobKnowldegeLabel.Name = "JobKnowldegeLabel";
+            this.JobKnowldegeLabel.Size = new System.Drawing.Size(80, 13);
+            this.JobKnowldegeLabel.TabIndex = 10;
+            this.JobKnowldegeLabel.Text = "Job Knowldege";
+            // 
+            // JobKnowledgeComboBox
+            // 
+            this.JobKnowledgeComboBox.FormattingEnabled = true;
+            this.JobKnowledgeComboBox.Location = new System.Drawing.Point(449, 43);
+            this.JobKnowledgeComboBox.Name = "JobKnowledgeComboBox";
+            this.JobKnowledgeComboBox.Size = new System.Drawing.Size(317, 21);
+            this.JobKnowledgeComboBox.TabIndex = 11;
+            // 
+            // ProductivityLabel
+            // 
+            this.ProductivityLabel.AutoSize = true;
+            this.ProductivityLabel.Location = new System.Drawing.Point(446, 306);
+            this.ProductivityLabel.Name = "ProductivityLabel";
+            this.ProductivityLabel.Size = new System.Drawing.Size(62, 13);
+            this.ProductivityLabel.TabIndex = 12;
+            this.ProductivityLabel.Text = "Productivity";
+            // 
+            // ProductividyComboBox
+            // 
+            this.ProductividyComboBox.FormattingEnabled = true;
+            this.ProductividyComboBox.Location = new System.Drawing.Point(449, 323);
+            this.ProductividyComboBox.Name = "ProductividyComboBox";
+            this.ProductividyComboBox.Size = new System.Drawing.Size(317, 21);
+            this.ProductividyComboBox.TabIndex = 13;
+            // 
+            // AttitudeLabel
+            // 
+            this.AttitudeLabel.AutoSize = true;
+            this.AttitudeLabel.Location = new System.Drawing.Point(27, 593);
+            this.AttitudeLabel.Name = "AttitudeLabel";
+            this.AttitudeLabel.Size = new System.Drawing.Size(43, 13);
+            this.AttitudeLabel.TabIndex = 14;
+            this.AttitudeLabel.Text = "Attitude";
+            // 
+            // AttitudeComboBox
+            // 
+            this.AttitudeComboBox.FormattingEnabled = true;
+            this.AttitudeComboBox.Location = new System.Drawing.Point(27, 610);
+            this.AttitudeComboBox.Name = "AttitudeComboBox";
+            this.AttitudeComboBox.Size = new System.Drawing.Size(317, 21);
+            this.AttitudeComboBox.TabIndex = 15;
+            // 
+            // CommunicationSkillsLabel
+            // 
+            this.CommunicationSkillsLabel.AutoSize = true;
+            this.CommunicationSkillsLabel.Location = new System.Drawing.Point(449, 593);
+            this.CommunicationSkillsLabel.Name = "CommunicationSkillsLabel";
+            this.CommunicationSkillsLabel.Size = new System.Drawing.Size(106, 13);
+            this.CommunicationSkillsLabel.TabIndex = 16;
+            this.CommunicationSkillsLabel.Text = "Communication Skills";
+            // 
+            // CommunicationSkillsComboBox
+            // 
+            this.CommunicationSkillsComboBox.FormattingEnabled = true;
+            this.CommunicationSkillsComboBox.Location = new System.Drawing.Point(449, 610);
+            this.CommunicationSkillsComboBox.Name = "CommunicationSkillsComboBox";
+            this.CommunicationSkillsComboBox.Size = new System.Drawing.Size(317, 21);
+            this.CommunicationSkillsComboBox.TabIndex = 17;
+            // 
+            // SumbitEvaluationButton
+            // 
+            this.SumbitEvaluationButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.SumbitEvaluationButton.Location = new System.Drawing.Point(288, 876);
+            this.SumbitEvaluationButton.Name = "SumbitEvaluationButton";
+            this.SumbitEvaluationButton.Size = new System.Drawing.Size(220, 42);
+            this.SumbitEvaluationButton.TabIndex = 18;
+            this.SumbitEvaluationButton.Text = "Submit Evaluation";
+            this.SumbitEvaluationButton.UseVisualStyleBackColor = false;
+            // 
+            // Employee_Evaluation_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(812, 930);
+            this.Controls.Add(this.SumbitEvaluationButton);
+            this.Controls.Add(this.CommunicationSkillsComboBox);
+            this.Controls.Add(this.CommunicationSkillsLabel);
+            this.Controls.Add(this.AttitudeComboBox);
+            this.Controls.Add(this.AttitudeLabel);
+            this.Controls.Add(this.ProductividyComboBox);
+            this.Controls.Add(this.ProductivityLabel);
+            this.Controls.Add(this.JobKnowledgeComboBox);
+            this.Controls.Add(this.JobKnowldegeLabel);
+            this.Controls.Add(this.AttendanceComboBox);
+            this.Controls.Add(this.AttendanceLabel);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.ReviewerTitleLabel);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.ReviewerNameLabel);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.EvaluationDateLabel);
+            this.Controls.Add(this.EmployeeIDTextBox);
+            this.Controls.Add(this.EmployeeIDLabel);
+            this.Name = "Employee_Evaluation_Form";
+            this.Text = "Employee_Evaluation_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EmployeeIDLabel;
+        private System.Windows.Forms.RichTextBox EmployeeIDTextBox;
+        private System.Windows.Forms.Label EvaluationDateLabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label ReviewerNameLabel;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label ReviewerTitleLabel;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label AttendanceLabel;
+        private System.Windows.Forms.ComboBox AttendanceComboBox;
+        private System.Windows.Forms.Label JobKnowldegeLabel;
+        private System.Windows.Forms.ComboBox JobKnowledgeComboBox;
+        private System.Windows.Forms.Label ProductivityLabel;
+        private System.Windows.Forms.ComboBox ProductividyComboBox;
+        private System.Windows.Forms.Label AttitudeLabel;
+        private System.Windows.Forms.ComboBox AttitudeComboBox;
+        private System.Windows.Forms.Label CommunicationSkillsLabel;
+        private System.Windows.Forms.ComboBox CommunicationSkillsComboBox;
+        private System.Windows.Forms.Button SumbitEvaluationButton;
+    }
+}
